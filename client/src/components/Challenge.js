9a3a862cedab2361f23a5a39ed1940de7970f0aa
Kyle Ferrigan <kbferrigan@icloud.com>
2022-03-05 19:49:33 -0600

    Final

--- client/src/components/Challenge.js
+++ client/src/components/Challenge.js
@@ -1,7 +1,70 @@
+async function getLowStockItems() {//asych due to fetch request
+    console.log("Attempting to fetch low-stock items") //DEBUG
+
+    //Fetch request
+    let res = await fetch('http://localhost:4567/low-stock', {method: 'GET'})
+    let data = await res.text()
+    let pData = JSON.parse(data);
+    //console.log(pData); //DEBUG
+
+    //Output parsed json data to frontend
+    var table = document.getElementById("stockTable").getElementsByTagName('tbody')[0];
+    table.innerHTML = "";//clear table in case get low stock button is pressed more than once
+    for(var i = 0; pData.at(i) != null; i++){ //Make table and insert data
+        var row = table.insertRow();
+        var column0 = row.insertCell(0);
+        var column1 = row.insertCell(1);
+        var column2 = row.insertCell(2);
+        var column3 = row.insertCell(3);
+        var column4 = row.insertCell(4);
+        column0.innerHTML = '<text id = sku'+ i +'>' + pData.at(i).id + '</text>' //added id to be able to track later
+        column1.innerText = pData.at(i).name;
+        column2.innerText = pData.at(i).stock;
+        column3.innerText = pData.at(i).capacity;
+        column4.innerHTML = '<input type="text" id="fname'+ i +'" name="fname">'; //insert textbox for data entry unique id for easier extraction in re-order cost
+    }
+}
+async function getReorderCost(){ //async due to fetch request
+    console.log("Attempting to fetch restock-cost")
+
+    //Map skus to amount ordered
+    let skumap = new Map();
+    for(let i = 0; document.getElementById("sku"+i.toString()) != null; i++){ //keep iterating through all textboxes
+        //console.log(document.getElementById("sku"+i.toString()).innerText + " : " + document.getElementById("fname"+i.toString()).value); //DEBUG
+        skumap.set(document.getElementById("sku"+i.toString()).innerText, document.getElementById("fname"+i.toString()).value); //Get sku and value from textboxes and map them together
+    }
+
+
+    //Convert from map to JSON
+    var obj =  Object.fromEntries(skumap);
+    var json = JSON.stringify(obj);
+
+
+    //Fetch request to backend
+    let res = await fetch('http://localhost:4567/restock-cost',{
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: json
+    });
+    let data = await res.text()
+    //console.log(data) DEBUG
+
+
+    //Output Data to Frontend
+    if (!isNaN(Number(data))){//Determine if data is a number
+        document.getElementById("totalCost").innerText = "Total Cost: $" +  Number(data).toFixed(2);//Cutoff at two decimal points for cents
+    }else{//if not display message by itself
+        document.getElementById("totalCost").innerText = data;
+    }
+}
+
 export default function Challenge() {
   return (
     <>
-      <table>
+      <table id="stockTable">
         <thead>
           <tr>
             <td>SKU</td>
@@ -13,19 +76,17 @@ export default function Challenge() {
         </thead>
         <tbody>
           {/* 
-          TODO: Create an <ItemRow /> component that's rendered for every inventory item. The component
+          Create an <ItemRow /> component that's rendered for every inventory item. The component
           will need an input element in the Order Amount column that will take in the order amount and 
           update the application state appropriately.
           */}
         </tbody>
       </table>
-      {/* TODO: Display total cost returned from the server */}
-      <div>Total Cost: </div>
-      {/* 
-      TODO: Add event handlers to these buttons that use the Java API to perform their relative actions.
-      */}
-      <button>Get Low-Stock Items</button>
-      <button>Determine Re-Order Cost</button>
+      {/* Display total cost returned from the server */}
+      <div id = "totalCost">Total Cost: </div>
+      {/* Add event handlers to these buttons that use the Java API to perform their relative actions. */}
+      <button onClick={getLowStockItems}>Get Low-Stock Items</button>
+      <button onClick={getReorderCost}>Determine Re-Order Cost</button>
     </>
   );
 }
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,3 +1,11 @@
+//import netscape.javascript.JSObject; Note: Causes issues on MacOS
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.ooxml.*;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import java.io.*;
+import java.util.*;
 import static spark.Spark.*;
 
 public class Main {
@@ -20,14 +28,179 @@ public class Main {
         //This is required to allow the React app to communicate with this API
         before((request, response) -> response.header("Access-Control-Allow-Origin", "http://localhost:3000"));
 
-        //TODO: Return JSON containing the candies for which the stock is less than 25% of it's capacity
+        //Return JSON containing the candies for which the stock is less than 25% of it's capacity
         get("/low-stock", (request, response) -> {
-            return null;
+            System.out.println("Fetching low-stock items"); //DEBUG
+
+            JSONArray lowStockList = new JSONArray(); //Container for JSON to be returned
+
+            try{
+                FileInputStream file = new FileInputStream(new File("resources/Inventory.xlsx"));
+                Workbook workbook = new XSSFWorkbook(file);
+                Sheet sheet = workbook.getSheetAt(0);
+                Iterator<Row> iterator = sheet.iterator();
+                Row currentRow = iterator.next(); // skip reading the first row which is just the headers
+
+                while (iterator.hasNext()) {
+                    currentRow = iterator.next();
+                    Iterator<Cell> cellIterator = currentRow.iterator();
+                    JSONObject item = new JSONObject(); //Create new JSON object to store item values in
+
+                    for (int i = 0; cellIterator.hasNext(); i++) {
+                        Cell currentCell = cellIterator.next();
+
+                        if (currentCell.getCellType() == CellType.STRING) {
+                            //System.out.print(currentCell.getStringCellValue() + " "); //console DEBUG
+                            item.put("name", currentCell.getStringCellValue());
+                        }
+
+                        else if (currentCell.getCellType() == CellType.NUMERIC) {
+                            //System.out.print(currentCell.getNumericCellValue() + " "); //console DEBUG
+                            switch (i){//Store JSON values
+                                case 1: //Stock
+                                    item.put("stock", currentCell.getNumericCellValue());
+                                    break;
+
+                                case 2: //Capacity
+                                    item.put("capacity", currentCell.getNumericCellValue());
+                                    break;
+
+                                case 3: //ID
+                                    item.put("id", currentCell.getNumericCellValue());
+                                    break;
+                            }
+                        }
+                    }
+
+                    if ((double)item.get("stock") < ((double)item.get("capacity")) * 0.25){ //if item stock is less then a quarter capcity add to low stock list
+                        lowStockList.add(item);
+                    }
+                    //System.out.println();//console DEBUG
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return lowStockList;
         });
 
-        //TODO: Return JSON containing the total cost of restocking candy
+
+
+
+        //Return JSON containing the total cost of restocking candy
         post("/restock-cost", (request, response) -> {
-            return null;
+            System.out.println("Fetching restock cost"); //DEBUG
+            double restockCost = 0;
+            //System.out.println(request.body()); //DEBUG
+
+            //Get keys(SKU/ID) from POST request
+            JSONParser parser = new JSONParser();
+            JSONObject jObj = (JSONObject) parser.parse(request.body());
+            Set kSet = jObj.keySet();
+
+
+            //SKU:Price Maps
+            Map<Integer,Double> candycorp = new HashMap();
+            Map<Integer,Double> theSweetSuite = new HashMap();
+            Map<Integer,Double> dentistsHateUs = new HashMap();
+
+            //open Excel and fill out SKU:Price maps
+            try{
+                FileInputStream file = new FileInputStream(new File("resources/Distributors.xlsx"));
+                Workbook workbook = new XSSFWorkbook(file);
+
+                ArrayList<Sheet> wbook = new ArrayList<>(); //make list for easier iteration and reusable code
+                wbook.add(workbook.getSheetAt(0));
+                wbook.add(workbook.getSheetAt(1));
+                wbook.add(workbook.getSheetAt(2));
+
+
+                for (int i = 0; i<3; i++){
+                    Iterator<Row> iterator = wbook.get(i).iterator();
+                    Row currentRow = iterator.next(); // skip reading the first row which is just the headers
+                    while (iterator.hasNext()) {
+                        currentRow = iterator.next();
+                        Iterator<Cell> cellIterator = currentRow.iterator();
+                        int sku = 0;
+                        double cost = 0;
+                        for (int j = 0; cellIterator.hasNext(); j++) {
+                            Cell currentCell = cellIterator.next();
+                            if (currentCell.getCellType() == CellType.NUMERIC) {
+                                //System.out.print(currentCell.getNumericCellValue() + " "); //console DEBUG
+                                switch (j){//Store JSON values
+                                    case 1: //SKU
+                                        sku = ((int) currentCell.getNumericCellValue());
+                                        break;
+
+                                    case 2: //COST
+                                        cost = ((double) currentCell.getNumericCellValue());
+                                        break;
+                                }
+                            }
+                        }
+
+                        switch(i){//change what map to put into depending on current workbook
+                            case 0:
+                                candycorp.put(sku,cost);
+                                break;
+                            case 1:
+                                theSweetSuite.put(sku,cost);
+                                break;
+                            case 2:
+                                dentistsHateUs.put(sku,cost);
+                                break;
+                        }
+                        //System.out.println();//console DEBUG
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+            for(Object keys : kSet){
+                //System.out.println(keys); //DEBUG
+                //Use keys to find amount needed here and then find what the lowest cost is
+                int sku = Integer.valueOf((String) keys);
+                //System.out.println(sku); //DEBUG
+
+                double cc = 999999999;
+                double tss = 999999999;
+                double dhu = 999999999;
+
+                //Inexpensive way of filtering out null
+                if (candycorp.get(sku) == null) {
+                    cc = 999999999;
+                }else{
+                    cc = candycorp.get(sku);
+                }
+
+                if (theSweetSuite.get(sku) == null) {
+                    tss = 999999999;
+                }else{
+                    tss = theSweetSuite.get(sku);
+                }
+
+                if (dentistsHateUs.get(sku) == null) {
+                    dhu = 999999999;
+                }else {
+                    dhu = dentistsHateUs.get(sku);
+                }
+                try{//add the minimum cost item times the amount of the item, round to nearest 100th
+                    restockCost += (Integer.parseInt((String)jObj.get(Integer.toString(sku))) * Math.min(cc,Math.min(tss,dhu)));
+                }catch (Exception NumberFormatException){
+                    return "ERROR: Please enter integers into all fields";
+                }
+
+
+            }
+            //System.out.println(restockCost); //DEBUG
+            return restockCost;
         });
 
     }
